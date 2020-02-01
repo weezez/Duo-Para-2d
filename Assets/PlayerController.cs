041781e70b1c5781c2913225542eac6c63ed061a
Metronoid <dbahnsen4@gmail.com>
2020-02-01 23:48:02 +0100

    Player Controller updated

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class WorldShift : MonoBehaviour
 {
+    public GameObject Player1;
+    public GameObject Player2;
+
+
+    public Camera cam;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+    
     }
 }
