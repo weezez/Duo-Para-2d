713a016a006117ab1a8a53c87d2c0c4b1ac7e089
Magnus <magnus@fotorummet.dk>
2020-02-02 00:32:24 +0100

    Nothing

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour { 
+
+    private Transform playerTransform;
+
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    playerTransform = GameObject.FindGameObjectsWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+
+        temp.x = playerTransform.position.x;
+
+        temp.x += offset;
+
+        transform.position = temp;
+    }
+}
