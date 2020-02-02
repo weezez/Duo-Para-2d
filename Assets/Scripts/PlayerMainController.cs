cb615343e336ee63ab72c9bc61646e3ea62572e0
Metronoid <dbahnsen4@gmail.com>
2020-02-02 01:01:22 +0100

    Player Controller Updated

---
+++ Assets/Scripts/PlayerMainController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayerController : MonoBehaviour
+{
+    //public GameObject Player1;
+    //public GameObject Player2;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // if (world1.activeSelf && Input.GetKeyDown(KeyCode.Space)) {
+            //world1.SetActive(false); // Set one world to dissappear.
+            //world2.SetActive(true); //Set one world to appear.
+            //cam.backgroundColor = colorPast; 
+        //} else if (world2.activeSelf && Input.GetKeyDown(KeyCode.Space)) {
+        //    world1.SetActive(true);
+        //    world2.SetActive(false);
+        //    cam.backgroundColor = colorPresent;
+        //}
+
+        //if(Player1.CompareTag("main"))
+    }
+}
